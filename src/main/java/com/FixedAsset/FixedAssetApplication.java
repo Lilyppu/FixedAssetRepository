2ae92f557e6c641847389ac0704a1798fe187bb6
Lilyppu <lily@ppu.co.id>
2024-05-17 11:56:12 +0700

    first commit

---
+++ src/main/java/com/FixedAsset/FixedAssetApplication.java
@@ -0,0 +1,13 @@
+package com.FixedAsset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedAssetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedAssetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FixedAsset/FixedAssetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FixedAsset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixedAssetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
